241bfcf6864b161c2875b81db8133a174e8f8266
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-02-23 19:41:38 +0000

    rdgthf

--- Flask_esercizio_1.py
+++ Flask_esercizio_1.py
@@ -3,6 +3,13 @@
 # la seconda deve contenere un libro e la terza deve contenere un calendario.
 # Utilizzare un file css per definire la grafica della pagina.
 
+#2. modificare il server precedente per far sì che quando l'utente clicca sulla prima immagine vengano fornite le previsioni 
+# del tempo.
+# Visto che, comunque, le previsioni dei vari servizi metereologici sono sempre sbagliate,
+#il nostro server genera un numero casuale compreso tra 0 e 8: se il numero è minore di 2 la previsione è "pioggia",
+# se è compreso tra 3 e 5 la previsione è "nuvoloso", se è maggiore di 5 la previsione è "sole".
+# Abbinare ad ogni previsione un'immagine adatta. Utilizzare un css per definire la grafica.
+# La route per accedere al serizio deve essere /meteo
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -11,8 +18,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template("html_per_Flask_esercizio_1.html")
-
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
