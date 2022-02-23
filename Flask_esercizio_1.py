e07f8c2dfbf2d08317c321d50a1b626ba0cbea78
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-02-23 13:54:30 +0000

    save

---
+++ Flask_esercizio_1.py
@@ -0,0 +1,19 @@
+#   1. Realizzare un server web che come home page presenti tre immagini della stessa dimensione una di fianco all'altra.
+# La prima immagine deve avere a che fare con le previsioni del tempo,
+# la seconda deve contenere un libro e la terza deve contenere un calendario.
+# Utilizzare un file css per definire la grafica della pagina.
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template("html_per_Flask_esercizio_1.html")
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+
--- app2.py
+++ app2.py
@@ -3,13 +3,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-import time
-start_time = time.time()
+
+#  per il tempo
+import datetime
+# hour = datetime.datetime.now().hour
 
 @app.route('/', methods=['GET'])
-#def colore_sfondo():
-#    return (start_time)
-print(start_time)
+def time_sfondocol():
+  hour = datetime.datetime.now().hour
+  if hour%2 == 0:
+    col = "green"
+  else:
+    col = "red"
+  return render_template("per_colore_sfondo.html", colore = col, testo = "hello, World!", ore = hour)
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
