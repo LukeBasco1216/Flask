9ac56e47a508f34272705441e8374e5c858c35f0
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-04-07 09:51:41 +0000

    ok

---
+++ correzioneVer/correzioneFilaC/codice.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request
+app = Flask(__name__)
+import pandas as pd
+
+import io
+import geopandas as gpd
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+comuni = gpd.read_file("/workspace/Flask/EsercizioInPrepVer(1)/files/Com01012021_g_WGS84.zip")
+province = gpd.read_file("/workspace/Flask/EsercizioInPrepVer(1)/files/ProvCM01012021_g_WGS84.zip")
+regioni = gpd.read_file("/workspace/Flask/EsercizioInPrepVer(1)/files/Reg01012021_g_WGS84.zip")
+
+@app.route('/', methods=['GET'])
+def HomeP():
+  
+  return render_template("home.html")
+
+@app.route('/homedata', methods=['GET'])
+def homedata():
+    es = request.args["esercizio"]
+    if es == "Esercizio1":
+        return render_template("inserimentoval.html")
+    elif es == "Esercizio2":
+        return render_template("inputquartiere.html")
+ 
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
