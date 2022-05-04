10c48509d9624b8089a08c5cf3a994cd9e33afe0
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-05-04 12:57:42 +0000

    ok

--- projectfineanno/codice.py
+++ projectfineanno/codice.py
@@ -25,16 +25,16 @@ from folium import plugins
 
 hotellombardia = pd.read_csv("/workspace/Flask/projectfineanno/files/Regione-Lombardia---Mappa-delle-strutture-ricettive.csv", sep=";", encoding='ISO-8859-1', on_bad_lines='skip')
 
-# @app.route('/', methods=['GET'])
-# def HomeP():
-#   alberghi = hotellombardia[hotellombardia.Categoria == "Alberghiere"]
-#   return render_template("homepage.html")
-
 @app.route('/', methods=['GET'])
 def HomeP():
-  m = folium.Map(location=[45.5236, -122.6750])
-  m.save("testfolium.html")
-  return m
+  alberghi = hotellombardia[hotellombardia.Categoria == "Alberghiere"]
+  return render_template("homepage.html")
+
+# @app.route('/', methods=['GET'])
+# def HomeP():
+#   m = folium.Map(location=[45.5236, -122.6750])
+#   m.save("testfolium.html")
+#   return m
 
 
 
